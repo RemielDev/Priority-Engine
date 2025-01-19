2ec4786d572cafcc4d3c4114eacc79d19a53e243
Remiel Shirazi <81453063+RemielDev@users.noreply.github.com>
2025-01-18 16:20:24 -0800

    Create ControllerTemplate.lua

---
+++ ReplicatedStorage/ClientControllers/ControllerTemplate.lua
@@ -0,0 +1,19 @@
+local Controller = {
+	Priority = 0;
+	_ignored = false;
+}
+
+--[[ Controller Description: 
+
+]]
+
+-- Initialization
+
+function Controller.run()
+	-- This is where code will run
+	-- Yielding on the the run function will yield the continuation of loading the next priority threads until suspended/finished
+	--     It will not yield threads on the same priority level
+	print('Priority 0: Client has begun loading controllers')
+end
+
+return Controller
